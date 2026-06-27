0309665975bf1c561b8fb38432f39365a791e3d2
zunnurain786 <136262230+zunnurain786@users.noreply.github.com>
2026-06-27 10:59:08 +0530

    Improve database connection logging in main application
    Enhanced database connection logging in the application.

--- src/main/java/com/recurring_deposit/RecurringDepositApplication.java
+++ src/main/java/com/recurring_deposit/RecurringDepositApplication.java
@@ -1,9 +1,13 @@
 package com.recurring_deposit;
 
+import javax.sql.DataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RecurringDepositApplication {
@@ -14,22 +18,27 @@ public class RecurringDepositApplication {
 
     public static void main(String[] args) {
 
-
         SpringApplication.run(
                 RecurringDepositApplication.class,
                 args);
 
         logger.info(
                 "Recurring Deposit Application Started Successfully");
+    }
 
-        @Bean
-CommandLineRunner checkDb(DataSource dataSource) {
-    return args -> {
-        System.out.println(
-            dataSource.getConnection().getMetaData().getURL()
-        );
-    };
-}
+    @Bean
+    CommandLineRunner checkDb(DataSource dataSource) {
+        return args -> {
 
+            System.out.println("====================================");
+            System.out.println("Connected Database URL:");
+            System.out.println(
+                    dataSource.getConnection()
+                            .getMetaData()
+                            .getURL()
+            );
+            System.out.println("====================================");
+
+        };
     }
 }
